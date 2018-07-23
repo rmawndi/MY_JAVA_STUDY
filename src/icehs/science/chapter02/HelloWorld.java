b21b110599fce1d32ce2dd5b94225f4753959611
admin <admin@10.211.1.32>
2018-07-23 12:23:32 +0900

    Java의 주석은 3가지가 있움 // /*~*/ /**~*/

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,7 +1,18 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
+	/**
+	 * Java야 놀자
+	 * @param args
+	 */
+	/*
+	 * 이 프로그램은 선인고등학교 Java 응용프로그래밍 수업에서 만들어졌습니다.
+	 * 만든사람: 이금주
+	 * 만든날짜: 2018년 7월 23일
+	 * 버전 : 0.1
+	 */
 public static void main(String[] args) {
+	//나의 첫번째 프로그램
 	System.out.println("Hello World!");
 	System.out.println("Hello 이금주");
 }
